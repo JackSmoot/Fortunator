9938a7efb58a86ee9f7668100987bfbe9b46763e
JackSmoot <smooja01@gettysburg.edu>
2025-03-25 19:37:07 -0400

    Init Commit + UI init

--- Fortunator/ContentView.swift
+++ Fortunator/ContentView.swift
@@ -10,15 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Fortune Cookie")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+                .padding()
+            
+            Image("fortunecookie")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                .padding()
+                        
+            Button(action: {
+                print("Fortune told!")
+            }) {
+                Text("Get Fortune")
+                    .font(.title2)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.black)
+                    .cornerRadius(30)
+            }
+            .padding()
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
