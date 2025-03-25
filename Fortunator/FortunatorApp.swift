593c1acfabdb3be505ef4cc1f2ba1a8903fe1147
JackSmoot <smooja01@gettysburg.edu>
2025-03-24 22:20:10 -0400

    Initial Commit

---
+++ Fortunator/FortunatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortunatorApp.swift
+//  Fortunator
+//
+//  Created by Jack Smoot on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FortunatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
